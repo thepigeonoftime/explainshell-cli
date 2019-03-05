c4fde24b2cb475f95f6367d59790e40ff30a5fca
p1g30n <anatol@riseup.net>
2019-03-05 15:45:24 +0100

    optimize delimiters

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use select::document::Document;
 use select::predicate::Attr;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let mut stderr = std::io::stderr();
+    let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
     if args.len() < 2 {
         writeln!(&mut stderr, "Usage: explain [COMMAND] [ARG 1]..[ARG N]\nExample: explain ls -l -a\n").expect("couldn't write to stderr");
         process::exit(1);
@@ -26,9 +26,9 @@ fn explain(query: String) {
     let url = format!("{}{}", base_url, query);
     let resp = reqwest::get(&url).unwrap();
     assert!(resp.status().is_success());
-    let delimiter = format!("\n{}\n", "_".repeat(50));
+    let delimiter = format!("\n{}", "_".repeat(50));
     Document::from_read(resp)
         .unwrap()
         .find(Attr("class", "help-box"))
-        .for_each(|x| println!("{}{}", x.text(), delimiter));
+        .for_each(|x| println!("\n{}{}", x.text(), delimiter));
 }
